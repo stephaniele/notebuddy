28fd50790d27072dd766fb123574546e1d180da2
stephaniele <stephaniele147@gmail.com>
2021-11-18 20:27:43 -0600

    update local

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route("/") 
+def index():
+    return ("Welcome to NoteBuddy")
+
+@app.route("/home")
+def home():
+	return("This is where your workspaces are")
+
+@app.route("/workspace")
+def workspace():
+	return("This is your workspace page")

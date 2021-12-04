347261d9dd93d10a56feb89a7268af5da692d35e
Carrie <nshao@macalester.edu>
2021-12-03 21:33:15 -0600

    fixed spacing

--- database.py
+++ database.py
@@ -1,22 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
 class Database:
-	def __init__(self, app):
-		db = SQLAlchemy(app)
-		self.db = db
-		self.user_model = UserModel(db)
-		self.file_model = FileModel(db)
+    def __init__(self, app):
+        db = SQLAlchemy(app)
+        self.db = db
+        self.user_model = UserModel(db)
+        self.file_model = FileModel(db)
         
 
 def UserModel(db):
-	class User(db.Model):
-		id = db.Column(db.Integer, primary_key=True)
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(100))
         email = db.Column(db.String(80), unique=True)
         password_hash = db.Column(db.String())
     
         def __init__(self, username, email, password):
-		    self.username = username
-		    self.email = email
-		    self.password = password
+            self.username = username
+            self.email = email
+            self.password = password
  
         def generate_hash(self, password):
             self.password_hash = generate_password_hash(password)
@@ -25,42 +27,42 @@ def UserModel(db):
             return check_password_hash(self.password_hash,password)
         
         def get(self, id=None):
-		    if id:
-			    return self.user_model.query.get(id)
-		    return self.user_model.query.all()
+            if id:
+                return self.user_model.query.get(id)
+            return self.user_model.query.all()
+  
+        def create(self, id, username, email):
+            user = self.user_model(id, username, email)
+            self.db.session.add(user)
+            self.db.session.commit()
 
-	    def create(self, id, username, email):
-	        user = self.user_model(id, username, email)
-		    self.db.session.add(user)
-		    self.db.session.commit()
+        def update(self, id, username, email):
+            user = self.get(id)
+            user.username = username
+            user.email = email
+            self.db.session.commit()
 
-	    def update(self, id, username, email):
-		    user = self.get(id)
-		    user.username = username
-		    user.email = email
-		    self.db.session.commit()
-
-	    def delete(self, id):
-		    id = self.get(id)
-		    self.db.session.delete(review)
-		    self.db.session.commit()
-	return User
+        def delete(self, id):
+            id = self.get(id)
+            self.db.session.delete(review)
+            self.db.session.commit()
+    return User
 
 def FileModel(db):
-	class File(db.Model):
-		__tablename__ = 'files'
-		id = db.Column('file_id', db.Integer, primary_key=True)
-		name = db.Column(db.String)
-		occupation = db.Column(db.String)
-		school = db.Column(db.String)
+    class File(db.Model):
+        __tablename__ = 'files'
+        id = db.Column('file_id', db.Integer, primary_key=True)
+        name = db.Column(db.String)
+        occupation = db.Column(db.String)
+        school = db.Column(db.String)
 
-		# many to one
-		owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'),
+        # many to one
+        owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'),
         nullable=False)
 
-		def __init__(self, name, email, occupation, school):
-			self.name = name
-			self.email = email
-			self.occupation = occupation
-			self.school = school
-	return File
+        def __init__(self, name, email, occupation, school):
+            self.name = name
+            self.email = email
+            self.occupation = occupation
+            self.school = school
+    return File
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from userdatabase import UserDatabase
+from database import Database
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userss.db'
@@ -20,13 +20,13 @@ def index():
 
 @app.route("/home")
 def home():
-	return("This is where your workspaces are")
+    return("This is where your workspaces are")
 
 @app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
-	if request.method == "POST":
-		return redirect('/home')
-	return render_template("accountform.html", action = "/sign_in", title = "Sign In")
+    if request.method == "POST":
+        return redirect('/home')
+    return render_template("accountform.html", action = "/sign_in", title = "Sign In")
 
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
@@ -37,11 +37,11 @@ def create_account():
         db.create(title, review, rating)
         return redirect('/')
     else:
-	    return render_template("accountform.html", action = "/create_account", title = "Create Account")
+        return render_template("accountform.html", action = "/create_account", title = "Create Account")
 
 @app.route("/workspace")
 def workspace():
-	return render_template("workspace.html")
+    return render_template("workspace.html")
 
 @login_manager.user_loader
 def load_user(user_id):

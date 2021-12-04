07db7e0cea9f5314e4ff7049e8da28a845b71ecc
Carrie <nshao@macalester.edu>
2021-12-03 21:20:43 -0600

    changed server, database and accountform

--- database.py
+++ database.py
@@ -1,66 +1,66 @@
-# class Database:
-# 	def __init__(self, app):
-# 		db = SQLAlchemy(app)
-# 		self.db = db
-# 		self.User = UserModel(db)
-# 		self.File = FileModel(db)
+class Database:
+	def __init__(self, app):
+		db = SQLAlchemy(app)
+		self.db = db
+		self.user_model = UserModel(db)
+		self.file_model = FileModel(db)
+        
 
-# 	# def get(self, id=None):
-# 	# 	if id:
-# 	# 		return self.model.query.get(id)
-# 	# 	return self.model.query.all()
+def UserModel(db):
+	class User(db.Model):
+		id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(100))
+        email = db.Column(db.String(80), unique=True)
+        password_hash = db.Column(db.String())
+    
+        def __init__(self, username, email, password):
+		    self.username = username
+		    self.email = email
+		    self.password = password
+ 
+        def generate_hash(self, password):
+            self.password_hash = generate_password_hash(password)
+     
+        def check_password(self, password):
+            return check_password_hash(self.password_hash,password)
+        
+        def get(self, id=None):
+		    if id:
+			    return self.user_model.query.get(id)
+		    return self.user_model.query.all()
 
-# 	# def create(self, title, text, rating):
-# 	# 	review = self.model(title, text, rating)
-# 	# 	self.db.session.add(review)
-# 	# 	self.db.session.commit()
+	    def create(self, id, username, email):
+	        user = self.user_model(id, username, email)
+		    self.db.session.add(user)
+		    self.db.session.commit()
 
-# 	# def update(self, id, title, text, rating):
-# 	# 	review = self.get(id)
-# 	# 	review.title = title
-# 	# 	review.text = text
-# 	# 	review.rating = rating
-# 	# 	self.db.session.commit()
+	    def update(self, id, username, email):
+		    user = self.get(id)
+		    user.username = username
+		    user.email = email
+		    self.db.session.commit()
 
-# 	# def delete(self, id):
-# 	# 	review = self.get(id)
-# 	# 	self.db.session.delete(review)
-# 	# 	self.db.session.commit()
+	    def delete(self, id):
+		    id = self.get(id)
+		    self.db.session.delete(review)
+		    self.db.session.commit()
+	return User
 
-# def UserModel(db):
-# 	class User(db.Model):
-# 		__tablename__ = 'users'
-# 		id = db.Column('user_id', db.Integer, primary_key=True)
-# 		name = db.Column(db.String)
-# 		email = db.Column(db.String)
-# 		occupation = db.Column(db.String)
-# 		school = db.Column(db.String)
+def FileModel(db):
+	class File(db.Model):
+		__tablename__ = 'files'
+		id = db.Column('file_id', db.Integer, primary_key=True)
+		name = db.Column(db.String)
+		occupation = db.Column(db.String)
+		school = db.Column(db.String)
 
-# 		# one to many 
-# 		files = db.relationship('File',backref='owner', lazy='select')
+		# many to one
+		owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'),
+        nullable=False)
 
-# 		def __init__(self, name, email, occupation, school):
-# 			self.name = name
-# 			self.email = email
-# 			self.occupation = occupation
-# 			self.school = school
-# 	return User
-
-# def FileModel(db):
-# 	class File(db.Model):
-# 		__tablename__ = 'files'
-# 		id = db.Column('file_id', db.Integer, primary_key=True)
-# 		name = db.Column(db.String)
-# 		occupation = db.Column(db.String)
-# 		school = db.Column(db.String)
-
-# 		# many to one
-# 		owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'),
-#         nullable=False)
-
-# 		def __init__(self, name, email, occupation, school):
-# 			self.name = name
-# 			self.email = email
-# 			self.occupation = occupation
-# 			self.school = school
-# 	return File
+		def __init__(self, name, email, occupation, school):
+			self.name = name
+			self.email = email
+			self.occupation = occupation
+			self.school = school
+	return File
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from userdatabase import UserDatabase
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userss.db'
@@ -11,7 +12,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # databases
-user_db = UserDatabase(app)
+db = Database(app)
 
 @app.route("/") 
 def index():
@@ -24,25 +25,19 @@ def home():
 @app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
 	if request.method == "POST":
-	    try:
-		  return redirect('/home')
-	    except:
-           return "Something went wrong redirecting"
+		return redirect('/home')
 	return render_template("accountform.html", action = "/sign_in", title = "Sign In")
 
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
-	if request.method == "POST":
-        try:
-           username = request.form["username"]
-           email = request.form["email"]
-           password = request.form["password"]
-           user_db.create(username, email, password)
-		   print(user_db.get(1))
-           return redirect('/home')
-        except:
-            return "Something went wrong adding the user to the database :("
-	return render_template("accountform.html", action = "/create_account", title = "Create Account")
+    if request.method == "POST":
+        title=request.form["title"]
+        review=request.form["review"]
+        rating=request.form["rating"]
+        db.create(title, review, rating)
+        return redirect('/')
+    else:
+	    return render_template("accountform.html", action = "/create_account", title = "Create Account")
 
 @app.route("/workspace")
 def workspace():

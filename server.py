424def09aaa3deba260e6596eb8feeae07788f62
Carrie <nshao@macalester.edu>
2021-12-06 19:28:24 -0600

    changed server.py

--- server.py
+++ server.py
@@ -1,45 +1,51 @@
 import os
 from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from database import Database
+from database import Database, userFactory
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config.from_pyfile('server.cfg')
 
 db = Database(app)
-
-@app.route("/") 
-def index():
-    return render_template("homepage.html")
-
-@app.route("/home")
-def home():
-    return("This is where your workspaces are")
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
     if request.method == "POST":
-        return redirect('/home')
+        email=request.form["email"]
+        user = userFactory.User.query.filter_by(email=email).first()
+        login_user(user)
+        return redirect('/homepage')
     return render_template("accountform.html", action = "/sign_in", title = "Sign In")
 
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
     if request.method == "POST":
-        title=request.form["title"]
-        review=request.form["review"]
-        rating=request.form["rating"]
-        db.create(title, review, rating)
-        return redirect('/')
+        username=request.form["username"]
+        email=request.form["email"]
+        password=request.form["password"]
+        db.createUser(username, email, password)
+        return redirect('/homepage')
     else:
         return render_template("accountform.html", action = "/create_account", title = "Create Account")
 
+@app.route("/homepage") 
+# @login_required
+def index():
+    return render_template("homepage.html")
+
 @app.route("/workspace")
+@login_required
 def workspace():
     return render_template("workspace.html")
 
 @login_manager.user_loader
 def load_user(user_id):
-    return UserModel.query.get(int(user_id))
+    return userFactory.query.get(int(user_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    

bc2574493dd64e312087826483d174aacf760044
qzhang1229 <qzhang1@macalester.edu>
2021-12-09 17:19:50 -0600

    update profile with ajax

---
+++ static/load_profile.js
@@ -0,0 +1,31 @@
+function load_profile(){
+   
+    
+
+    var username = document.getElementById("Username").value
+    var occupation = document.getElementById("Occupation").value
+    var school = document.getElementById("School").value
+    
+
+    var user_data = [
+        {"username":username},
+        {"occupation":occupation},
+        {"school":school}
+    ]
+    
+    $.ajax({
+        type: "POST",
+        url: "/edit_profile",
+        data: JSON.stringify(user_data),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result) {
+          var name = result.name;
+          var school = result.school
+          var occupation = result.occupation
+          document.getElementById("profile_name").innerHTML = name
+          document.getElementById("profile_school").innerHTML = school
+          $("#ProfileModal").modal("hide")
+        } 
+    });
+}
